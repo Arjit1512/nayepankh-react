6101fcfbab31788fb7d694ec0f5ee2da13456852
Arjit1512 <125079524+Arjit1512@users.noreply.github.com>
2023-11-07 00:31:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-    origin:["https://deploy-mern-1whq.vercel.app"],
+    origin:["https://nayepankh-react.vercel.app/"],
     methods:["POST","GET"],
     credentials:true
   }    
