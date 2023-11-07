c8ba7da1fa943c329d9a235fa1299a4f73cc888f
Arjit1512 <125079524+Arjit1512@users.noreply.github.com>
2023-11-07 10:37:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import User from "./models/User.js";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: "https://nayepankh-react-frontend.vercel.app/",
+  origin: "https://nayepankh-react-frontend.vercel.app",
   methods: ["POST", "GET"],
   credentials: true
 }));
