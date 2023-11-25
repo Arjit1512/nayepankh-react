e89de08efd14aecab012f8a2b2096aaa4b2d2529
Arjit1512 <125079524+Arjit1512@users.noreply.github.com>
2023-11-25 19:25:41 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -15,7 +15,7 @@ function Login() {
 
         try {
 
-            await axios.post("http://localhost:3001/auth/register",{//"https://nayepankh-react-arjit.vercel.app/auth/register", {
+            await axios.post("https://nayepankh-react-arjit.vercel.app/auth/register",{//"https://nayepankh-react-arjit.vercel.app/auth/register", {
                 email, password
             })
                    
